6e841b6a106e4fb08f5dfc33a10db6854d48995c
sanghun <vitash1215@gmail.com>
2024-12-08 23:35:06 +0900

    feat : before 13 weeks

--- funcPr-practice.js
+++ funcPr-practice.js
@@ -1,29 +1,39 @@
 const cart = [
-    { item: "노트북", price: 1200000, quantity: 1 },
-    { item: "마우스", price: 35000, quantity: 2 },
-    { item: "키보드", price: 89000, quantity: 1 },
+  { item: "laptop", price: 1200000, quantity: 1 },
+  { item: "mouse", price: 35000, quantity: 2 },
+  { item: "keyboard", price: 89000, quantity: 1 },
 ];
-    let totalPrice = 0;
-    for (let i=0; i<cart.length; i++){
-        totalPrice += cart[i].price * cart[i].quantity;
-    }
+let totalPrice = 0;
 
-    console.log(`Total Price: ${totalPrice}`);
+// for loop
+for (let i = 0; i < cart.length; i++) {
+  totalPrice += cart[i].price * cart[i].quantity;
+}
+console.log(`Total price: ${totalPrice}`);
 
-    totalPrice = 0;
-    cart.forEach(
-        item => {
-            totalPrice += item.price * item.quantity;
-        }
-    );
-    console.log(`Total Price: ${totalPrice}`);
+// forEach
+totalPrice = 0;
+cart.forEach((goods) => (totalPrice += goods.price * goods.quantity));
+console.log(`Total price: ${totalPrice}`);
 
-    totalPrice = cart.reduce( 
-        (total, item) => total + item.price * item.quantity, 0
-    );
-    console.log(`Total Price: ${totalPrice}`);
+// reduce
+totalPrice = cart.reduce((res, goods) => res + goods.price * goods.quantity, 0);
+console.log(`Total price: ${totalPrice}`);
 
-    const itemTotals = cart.map(
-        item => ({ item: item.item, total: item.price * item.quantity })
-    );
-    console.log(itemTotals);
+// map
+const itemTotals = cart.map((goods) => ({
+  item: goods.item,
+  total: goods.price * goods.quantity,
+}));
+console.log("Price totals:", itemTotals);
+
+// map to uppercase
+const names = ["alice", "bob", "charlie"];
+const upperCaseNames = names.map((name) => name.toUpperCase());
+console.log(upperCaseNames);
+
+// map to capitalize first letter
+const capitalStartNames = names.map(
+  (name) => name.charAt(0).toUpperCase() + name.slice(1)
+);
+console.log(capitalStartNames);
---
+++ greeting.js
@@ -0,0 +1,11 @@
+function greet(greeting) {
+  return function (name) {
+    console.log(greeting + "," + name);
+  };
+}
+
+//const greet=greeting=>name=>console.log(greeting+","+name);
+
+let hello = greet("hello");
+hello("lsh");
+hello("worlds");
